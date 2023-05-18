41c263e748f9a946fe9dafbe86a87aecabc94d36
chloe00000 <chloe@goofy.fr>
2023-05-18 11:20:43 +0200

    Membre

--- src/EspaceDeTravail/Liste.java
+++ src/EspaceDeTravail/Liste.java
@@ -12,19 +12,37 @@ public class Liste {
 	private ArrayList<Carte> sesCartes;
 	private int numListe;
 	private String nomListe;
+	private ArrayList<Membre> sesMenbre;
 
 	// Constructeur
 	public Liste(Tableau sonTableau) {
 		this.sonTableau = sonTableau;
 		sesCartes = new ArrayList<Carte>();
+		sesMenbre= new ArrayList<Membre>();
 		numListe = compteurNum++;
 		nomListe = "Nouvelle Liste";
 	}
-	public Liste(Tableau sonTableau, ArrayList<Carte> sesCartes, String nomListe) {
+	public Liste(Tableau sonTableau, ArrayList<Carte> sesCartes, String nomListe, ArrayList<Membre> sesMenbre) {
 		this.sonTableau = sonTableau;
 		this.sesCartes = sesCartes;
+		this.sesMenbre = sesMenbre;
 		this.numListe = compteurNum++;
 		this.nomListe = nomListe;
+	}
+
+	public Liste (String nomListe, Tableau sonTableau){
+		this.sonTableau = sonTableau;
+		sesCartes = new ArrayList<Carte>();
+		sesMenbre= new ArrayList<Membre>();
+		numListe = compteurNum++;
+		this.nomListe = nomListe;
+	}
+	public Liste (){
+		//this.sonTableau = ;
+		sesCartes = new ArrayList<Carte>();
+		sesMenbre= new ArrayList<Membre>();
+		numListe = compteurNum++;
+		this.nomListe = "nomListe";
 	}
 	
 	// Getter
@@ -53,6 +71,20 @@ public class Liste {
 
 	public void setNomListe(String nomListe) {
 		this.nomListe = nomListe;
+	}
+
+	public void ajouterMembre(Membre m){
+		this.sesMenbre.add(m);
+		for (Carte c:sesCartes ) {
+			c.ajouterMembre(m);
+		}
+	}
+
+	public void retirerMembre(Membre m){
+		this.sesMenbre.remove(m);
+		for (Carte c:sesCartes) {
+			c.retirerMembre(m);
+		}
 	}
 
 	// Methode
