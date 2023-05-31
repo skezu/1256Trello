fd2498c5cfdfb0b2a9d4addc6868b51866f26fb8
chloe00000 <chloe@goofy.fr>
2023-05-31 10:54:10 +0200

    test

--- src/Controller/MembreController.java
+++ src/Controller/MembreController.java
@@ -3,11 +3,33 @@ package Controller;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import EspaceDeTravail.Membre;
+import EspaceDeTravail.Liste;
+import EspaceDeTravail.Carte;
+import EspaceDeTravail.Tableau;
+
 
 public class MembreController extends JPanel implements ActionListener {
+    //action des boutton
+    private static final String ACTION_ADD_MEMBRE_CARTE = "ADD_MEMBRE_CARTE";
+    // conposante graphique
+    JButton _btnAjouterMembreCarte;
+    // autre
+    Carte c;
+
+    //constructeur
+    public MembreController(Carte c){
+        _btnAjouterMembreCarte = new JButton("Ajouter Membre");
+        _btnAjouterMembreCarte.setActionCommand(ACTION_ADD_MEMBRE_CARTE);
+        _btnAjouterMembreCarte.addActionListener(this);
+        add(_btnAjouterMembreCarte);
+        this.c=c;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals(ACTION_ADD_MEMBRE_CARTE)){
 
+        }
     }
 }
---
+++ src/Views/MembreView.java
@@ -0,0 +1,29 @@
+package Views;
+
+import EspaceDeTravail.Carte;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Views.MenuView;
+
+public class MembreView extends JDialog implements ActionListener {
+
+    //panel contenet les menbre
+    private JPanel PanelMembre;
+
+    //constrcteur
+    public MembreView(Carte c){
+
+    }
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
