b203ae36feccb5f6f85fce0469d104843ec520a0
chloe00000 <chloe@goofy.fr>
2023-05-31 09:59:38 +0200

    test

---
+++ src/Controller/MembreController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MembreController extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
